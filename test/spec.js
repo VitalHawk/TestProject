fb1b611868edbfa82746af022c829636e003cfe8
vit <vitalhawk@rambler.ru>
2015-07-03 18:15:10 +0200

    YYY

--- test/spec.js
+++ test/spec.js
@@ -4,4 +4,9 @@ describe("POW", function() {
   it("should be 2 in 3 power", function() {
     expect(pow(2,4)).toBe(16);
   }); 
+
+  it("should be 2 x 2", function() {
+    expect(2 * 2).toBe(4);
+  }); 
+
 });
