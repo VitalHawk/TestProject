b5908e285a3ac4fede2f12896dc1727b93a63baa
vit <vitalhawk@rambler.ru>
2015-07-03 18:19:46 +0200

    CCC

--- test/spec.js
+++ test/spec.js
@@ -4,4 +4,9 @@ describe("POW", function() {
   it("should be 2 in 3 power", function() {
     expect(pow(2,4)).toBe(16);
   }); 
+
+  it("should be 3 x 3", function() {
+    expect(3*3).toBe(9);
+  }); 
+
 });
